251648f75e9ac0f59f4bd2a8c1e89624464a6a8c
tmgAzit <azit.ghising@gmail.com>
2021-11-02 17:59:03 +1100

    loremIpsum generator component is defined

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length - 1) {
+      amount = data.length - 1;
+    }
+    setText(data.splice(0, amount));
+  };
+
+  return (
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraph:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          generate
+        </button>
+      </form>
+
+      {text.map((item, index) => {
+        return (
+          <article className='lorem-text' key={index}>
+            <p>{item}</p>
+          </article>
+        );
+      })}
+    </section>
+  );
+}
+
+export default App;
